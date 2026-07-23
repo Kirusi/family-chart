baf3c6d7f8ba04ccc5309492802db661b012a61a
Kirusi Msafiri <kirusi.msafiri@gmail.com>
2026-07-23 19:33:52 -0400

    Changed GV renderer to generate clusters between parents and families

--- family_chart/gv_renderer.py
+++ family_chart/gv_renderer.py
@@ -18,17 +18,44 @@ class GvRenderer:
         self.rows = rows
         self.relationships = relationships
 
-    def render(self) -> str:
+    def render(self) -> str:  # noqa: C901
         """Return a multiline GraphViz DOT string with people and families ordered as they appear in rows."""
+        all_relationships = {}
+        rendered_relationships = set()
+        for rel in self.relationships:
+            all_relationships[rel.lookup_key] = rel
         node_lines: list[str] = []
         for row in self.rows:
-            people = [person_w for people_list in row.get_people() for person_w in people_list]
-            for person_w in people:
-                node_lines.append(person_w.person.source)
-            families = [family_w for family_list in row.get_families() for family_w in family_list]
-            for family_w in families:
-                node_lines.append(family_w.family.source)
-        relationship_lines = [relationship.source for relationship in self.relationships]
+            for block in row.blocks:
+                people_ids = [person_w.id for person_w in block.people]
+                people_ids = sorted(people_ids)
+                family_ids = [family_w.id for family_w in block.families]
+                family_ids = sorted(family_ids)
+                cluster_id = "_".join([*people_ids, *family_ids])
+                node_lines.append(f'subgraph cluster_{cluster_id}\n{{\nstyle="invis";\n')
+                referenced_parents = set()
+                for person_w in block.people:
+                    node_lines.append(person_w.person.source)
+                for family_w in block.families:
+                    node_lines.append(family_w.family.source)
+                for family_w in block.families:
+                    family_id = family_w.id
+                    for parent_id in family_w.parents:
+                        rel_id = f"{parent_id}_{family_id}"
+                        rel = all_relationships.get(rel_id)
+                        if rel is None:
+                            raise ValueError(f"Cannot find relationship from '{parent_id}' to '{family_id}'")
+                        node_lines.append(rel.source)
+                        rendered_relationships.add(rel_id)
+                        referenced_parents.add(parent_id)
+                for person_w in block.people:
+                    if len(block.families) > 0 and person_w.id not in referenced_parents:
+                        raise ValueError(f"Parent '{person_w.id}' is not referenced in cluster '{cluster_id}'")
+                node_lines.append("}")
+        relationship_lines = []
+        for relationship in self.relationships:
+            if relationship.lookup_key not in rendered_relationships:
+                relationship_lines.append(relationship.source)
 
         sections = [
             "\n".join(self.settings.sources),
